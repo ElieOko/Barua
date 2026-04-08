b9f9b6ff18978f8d7dcbd32616f31e038bc85c4f
ElieOko <elieoko100@gmail.com>
2026-04-08 11:18:29 +0100

    feat(security->monitoring->sentry):module de monitoring track, log, crash avec sentry

---
+++ src/main/kotlin/emy/backend/barua/security/monitoring/SentryMonitor.kt
@@ -0,0 +1,18 @@
+package emy.backend.barua.security.monitoring
+
+import io.sentry.Sentry
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import jakarta.annotation.PostConstruct
+
+@Component
+class SentryInitializer(
+    @Value("\${sentry.dsn}") private val dsn: String
+) {
+    @PostConstruct
+    fun init() {
+        Sentry.init { options ->
+            options.dsn = dsn
+        }
+    }
+}
